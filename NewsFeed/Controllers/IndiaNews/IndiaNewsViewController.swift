6eec3fc0ec37cc49dabf381ba2a5932527295afd
Ashish <contactashish161@gmail.com>
2020-02-14 12:26:39 +0530

    Created basic flow of the Apps in the Storyboard

---
+++ NewsFeed/Controllers/IndiaNews/IndiaNewsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  IndiaNewsViewController.swift
+//  NewsFeed
+//
+//  Created by Ashish Kumar on 14/02/20.
+//  Copyright © 2020 Ashish Kumar. All rights reserved.
+//
+
+import UIKit
+
+class IndiaNewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
